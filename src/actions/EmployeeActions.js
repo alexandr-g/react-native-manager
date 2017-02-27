d38153b6d835c6224b17dac857a5b36e7719d2e1
Alexander Grischuk <alexgrischuk@gmail.com>
2017-02-27 21:06:19 +0100

    Add employee create action creator

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,15 @@
 import { EMPLOYEE_UPDATE } from './types';
 
-export default employeeUpdate = ({ prop, value }) => {
+export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  }
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  return {
+    type: EMPLOYEE_CREATE,
+    payload: { name, phone, shift }
   }
 };
