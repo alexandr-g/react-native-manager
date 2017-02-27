7958cfcd0afbd39f16c9bf7f4343a350d9fbedb1
Alexander Grischuk <alexgrischuk@gmail.com>
2017-02-27 21:06:55 +0100

    Handle onPress event on create button pressed

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift });
+  }
+
   render() {
     return (
       <Card>
@@ -43,7 +50,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -66,4 +73,4 @@ const mapSateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapSateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapSateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
